d7f445e49bf36da79bc9319baf5b8cce677eb602
hyh <ec.huyinghuan@gmail.com>
2017-04-25 10:29:00 +0800

    cache

--- src/index.go
+++ src/index.go
@@ -6,22 +6,27 @@ import (
 	"gopkg.in/kataras/iris.v6"
 	"gopkg.in/kataras/iris.v6/adaptors/cors"
 	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+	"gopkg.in/kataras/iris.v6/adaptors/sessions"
 	"gopkg.in/kataras/iris.v6/adaptors/view"
 )
 
 func main() {
 	app := iris.New()
+
+	sessionAdapt := sessions.New(sessions.Config{})
+
 	app.Adapt(
 		iris.DevLogger(),
 		httprouter.New(),
+		sessionAdapt,
 		view.Handlebars("./views", ".hbs").Reload(true),
 		cors.New(cors.Options{AllowedOrigins: []string{"*"}}))
 
 	app.StaticWeb("/js", "./static/js")
 	app.StaticWeb("/css", "./static/css")
-	app.Get("/login", controller.LoginCtrl)
-	admin := app.Party("/admin", controller.authCtrl)
-	admin.Get("/admin/manager", controller.ManagerCtrl)
+
+	app.Party("/admin", controller.AuthCtrl).
+		Get("/manager", controller.ManagerCtrl)
 
 	app.Listen(":6300")
 }
